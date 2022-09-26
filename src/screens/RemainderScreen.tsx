2c001b5413537904f442c1e45141132cc552bf1a
HarishKing-venom <harishavenioc39111@gmail.com>
2022-09-26 12:39:11 +0530

    Remainder screen minor change

--- src/screens/RemainderScreen.tsx
+++ src/screens/RemainderScreen.tsx
@@ -2,15 +2,21 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { FC, useLayoutEffect, useState } from 'react';
 import { useNotification } from '../hooks/useNotifications';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
+
 
 const RemainderScreen: FC = () => {
-  const [rdata, setRdata] = useState();
+  const [rdata, setRdata] = useState(' ');
     const {getTriggerNotificationIds} = useNotification();
+    useFocusEffect(()=>{
+      findNotes();
+    })
       
   const findNotes = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem('alarmList');
       jsonValue != null ? JSON.parse(jsonValue) : null;
+      jsonValue!=null?setRdata(jsonValue):setRdata('Empty');
       console.warn(jsonValue);
       
     } catch(e) {
@@ -19,13 +25,14 @@ const RemainderScreen: FC = () => {
   }
   const clearAll = async () => {
     try {
-      await AsyncStorage.clear()
+      await AsyncStorage.clear();
     } catch(e) {
       // clear error
     }
   
     console.log('Done.')
   }
+  let mdata = rdata
   return (
     <View style={{margin:40}}>
       <Text>RemainderScreen Fetching Triggered Notifications </Text>
@@ -35,6 +42,7 @@ const RemainderScreen: FC = () => {
       </View>
       <View style={{margin:20}}>
         <Button title='Clear Triggers' onPress={clearAll} />
+        <Text>{rdata}</Text>
       </View>
     </View>
   )
